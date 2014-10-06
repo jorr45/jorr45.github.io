b5dcb06b0128922084983f6cc36c137c2ab86cd6
jorr45 <jorr45@users.noreply.github.com>
2014-10-06 16:20:05 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -71,7 +71,7 @@ jQuery(document).ready(function () {
        generateNamePage();//generate name page on page load
        $.getJSON("quiz.json", function(results){
                JSON = results;
-       }
+       });
                       
 });
 
