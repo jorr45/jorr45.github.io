86f15614fb076bf420853abc7bf67906a2f27198
jorr45 <jorr45@users.noreply.github.com>
2014-10-09 14:45:25 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -14,8 +14,7 @@ var title="A";
 jQuery(document).ready(function () {
          
        generateNamePage();//generate name page on page load
-       $.getJSON("quiz.json", function(results){
-              alert("A");
+       $.getJSON("quiz.json", function(data){
                JSON = results;
                console.log(JSON.title);
                questions=JSON.questions;
