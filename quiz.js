8a2b92982c5204a0c177215bd067133c9e05e442
jorr45 <jorr45@users.noreply.github.com>
2014-10-14 13:39:32 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -143,7 +143,7 @@ function submitQuiz(form){
             var k=scores.length-1;
             for (i=0; i<scores.length; i++){//place score in order
                     if (correctNumbers.length>=scores[i][0]){
-                            for (k = scores.length-1; k>i; k--){
+                            for (k = scores.length-1; k>=i; k--){
                                     scores[k]=scores[k-1];//move all lower scores down 1
                                     console.log("k loop");
                             }
