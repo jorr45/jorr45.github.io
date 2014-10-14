2c13970d1f45ac8123eca109dcfc25725eecb900
jorr45 <jorr45@users.noreply.github.com>
2014-10-14 08:24:55 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -15,6 +15,9 @@ var questions=[{
 var title="A";
 
 function generateNamePage(){//generate name page
+    if (JSON.recentUser === undefined){
+            JSON.recentUser = "";
+    }
     $("#questions").append('<form><h>Username: </h><input type="text" name="Name" value="'+JSON.recentUser+'"/><br><h>Password: </h><input type="text" name="Pass"/><br><br><input type="button" value="Login" onClick = "login(this.form)"/><input type="button" value="Sign Up" onClick = "signup(this.form)"/></form>');
 }
 
